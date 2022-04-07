22f016048295393543434311aba99d504fb8eba3
NBin17 <dkhoa1795@gmail.com>
2022-04-07 22:59:19 +0700

    mainScreen

---
+++ QuanLyBanVeChuyenBay/QuanLyBanVeChuyenBay/ThemeColor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyBanVeChuyenBay
+{
+    public static class ThemeColor
+    {
+        public static List<string> ColorList = new List<string>() { "#9BCCFE", "#004069", "#B2E2FF", "#74A6D6", "#6699C8" };
+    }
+}
